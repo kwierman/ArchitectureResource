167627590e3a5a428962a3dc06ae236ed11b086b
Kevin Wierman <kwierman@gmail.com>
2017-05-03 14:25:03 -0500

    Small changes

---
+++ architectureresource/settings/__init__.py
@@ -0,0 +1,118 @@
+network_list={
+    'vgg16a':{
+        'folder':'/data/drinkingkazu/SPClassification/vgg16a',
+        'test':'drinkingkazu_sp_vgg16a_test.prototxt',
+        'snapshot':'sp_vgg16a_iter_193750.caffemodel.h5',
+        'start_iter': 139500.0,
+        'stop_iter': 146000.0
+    },
+    'vgg16b':{
+        'folder':'/data/drinkingkazu/SPClassification/vgg16b',
+        'test':'drinkingkazu_sp_vgg16b_test.prototxt',
+        'snapshot':'sp_vgg16b_iter_114750.caffemodel.h5',
+        'start_iter': 113000.0,
+        'stop_iter': 114750.0
+    },
+    'vgg16c':{
+        'folder':'/data/drinkingkazu/SPClassification/vgg16c',
+        'test':'drinkingkazu_sp_vgg16c_test.prototxt',
+        'snapshot':'sp_vgg16c_iter_174000.caffemodel.h5',
+        'start_iter': 146750.0,
+        'stop_iter': 149250.0
+    },
+    'plainresnet10b':{
+        'folder':'/data/drinkingkazu/SPClassification/plainresnet10b',
+        'test':'drinkingkazu_sp_plainresnet10b_test.prototxt',
+        'snapshot':'sp_plainresnet10b_iter_410250.caffemodel.h5',
+        'start_iter': 117000.0,
+        'stop_iter': 119000.0
+    },
+    'plainresnet12b':{
+        'folder':'/data/drinkingkazu/SPClassification/plainresnet12b',
+        'test':'drinkingkazu_sp_plainresnet12b_test.prototxt',
+        'snapshot':'sp_plainresnet12b_iter_54000.caffemodel.h5',
+        'start_iter': 52250.0,
+        'stop_iter': 54000.0
+    },
+    'plainresnet10b_w4':{
+        'folder':'/data/drinkingkazu/SPClassification/plainresnet10b_w4',
+        'test':'drinkingkazu_sp_plainresnet10b_w4_test.prototxt',
+        'snapshot':'sp_plainresnet10b_w4_iter_242750.caffemodel.h5',
+        'start_iter': 151750.0,
+        'stop_iter': 154250.0
+    },
+    'resnet14b':{
+        'folder':'/data/drinkingkazu/SPClassification/resnet14b',
+        'test':'drinkingkazu_sp_resnet14b_test.prototxt',
+        'snapshot':'sp_resnet14b_iter_232028.caffemodel.h5',
+        'start_iter': 282250.0,
+        'stop_iter': 285250.0
+    },
+    'resnet14b_w4':{
+        'folder':'/data/drinkingkazu/SPClassification/resnet14b_w4',
+        'test':'drinkingkazu_sp_resnet14b_w4_test.prototxt',
+        'snapshot':'sp_resnet14b_w4_iter_195750.caffemodel.h5',
+        'start_iter': 192750.0,
+        'stop_iter': 195750.0
+    },
+    'plainresnet18b':{
+        'folder':'/data/drinkingkazu/SPClassification/plainresnet18b',
+        'test':'kwierman_sp_plainresnet18b_test.prototxt',
+        'snapshot':'sp_plainresnet18b_iter_105750.caffemodel.h5',
+        'start_iter': 104000.0,
+        'stop_iter': 105750.0
+    },
+    'plainresnet18b_w4':{
+        'folder':'/data/kwierman/plainresnet18b_w4',
+        'test':'kwierman_sp_plainresnet18b_w4_test.prototxt',
+        'snapshot':'sp_plainresnet18b_w4_iter_23750.caffemodel.h5',
+        'start_iter': 18250.0,
+        'stop_iter': 23750.0
+    },
+    'plainresnet20b':{
+        'folder':'/data/kwierman/plainresnet20b',
+        'test':'kwierman_sp_plainresnet20b_test.prototxt',
+        'snapshot':'sp_plainresnet20b_iter_254000.caffemodel.h5',
+        'start_iter': 248500.0,
+        'stop_iter': 254000.0
+    },
+    'plainresnet20b_w4':{
+        'folder':'/data/kwierman/plainresnet20b_w4',
+        'test':'kwierman_sp_plainresnet20b_w4_test.prototxt',
+        'snapshot':'sp_plainresnet20b_w4_iter_22750.caffemodel.h5',
+        'start_iter': 17250.0 , 
+        'stop_iter': 22750.0
+    },
+    'resnet29b':{
+        'folder':'/data/drinkingkazu/SPClassification/resnet29b',
+        'test':'drinkingkazu_sp_resnet29b_test.prototxt',
+        'snapshot':'sp_resnet29b_iter_397500.caffemodel.h5',
+        'start_iter': 428000.0,
+        'stop_iter': 433500.0
+    },
+    'resnet29b_w2':{
+        'folder':'/data/drinkingkazu/SPClassification/resnet29b_w2',
+        'test':'drinkingkazu_sp_resnet29b_w2_test.prototxt',
+        'snapshot':'sp_resnet29b_w2_iter_397500.caffemodel.h5',
+        'start_iter': 170500.0,
+        'stop_iter': 173500.0
+    },
+    'resnet29b_w4':{
+        'folder':'/data/drinkingkazu/SPClassification/resnet29b_w4',
+        'test':'drinkingkazu_sp_resnet29b_w4_test.prototxt',
+        'snapshot':'sp_resnet29b_w4_iter_397500.caffemodel.h5',
+        'start_iter':  339500.0,
+        'stop_iter': 344500.0
+    },
+    'resnet50b':{
+        'folder':'/data/drinkingkazu/SPClassification/resnet50b',
+        'test':'drinkingkazu_sp_resnet50b_test.prototxt',
+        'snapshot':'sp_resnet50b_iter_682250.caffemodel.h5',
+        'start_iter': 443750.0,
+        'stop_iter': 449250.0
+    },
+}
+
+plotter_data_dir='/data/shared/plotter_data'
+testA_directory='/data/shared/ArchitectureStudyInference/testA'
+inference_dir = '/data/shared/ArchitectureStudyInference/'
