1eb42c42aaa8ce74da2906ab2433527fde4c0c9c
Kevin Wierman <kwierman@gmail.com>
2017-05-03 14:32:27 -0500

    removed unnecessary code pieces

--- README.md
+++ README.md
@@ -5,12 +5,24 @@ Some Utility Functions for Doing Architecture Resource Utilization Studies
 
 The information fetched out by this script is produced in the [More IO branch of the Larbys Caffe fork](https://github.com/LArbys/caffe/tree/more_io_thread).
 
+Additionally, the pip version will need to be upgraded. I typically do this in my user directory in order to not mess with other users.
+
+~~~ bash
+pip install --user --upgrade setuptools pip
+~~~
+
 # Installation
 
 You REALLY want to make sure that this doesn't go into the system directory, so use this command EXACTLY. That is, unless you're working in a `virtualenv` ... with ROOT... 
 
 ~~~ bash
   pip install --user git+https://github.com/kwierman/ArchitectureResource
+~~~
+
+If you get a message about not being able to find `egg-info` for the package, then `setuptools` needs to be updated. Follow the pip install procedure from Requirements, and thens enter in the following command.
+
+~~~ bash
+  ~/.local/bin/pip install --user git+https://github.com/kwierman/ArchitectureResource
 ~~~
 
 ## For Developers
--- architectureresource/cli.py
+++ architectureresource/cli.py
@@ -4,11 +4,6 @@ import click
 import logging
 import sys
 import logging
-import tensorflow as tf
-from proton_decay_study.models.vgg16 import VGG16
-from proton_decay_study.generators.multi_file import MultiFileDataGenerator
-from proton_decay_study.generators.threaded_multi_file import ThreadedMultiFileDataGenerator
-from keras.callbacks import ModelCheckpoint, EarlyStopping
 import signal
 import sys
 
@@ -28,39 +23,6 @@ def get_resource_for_network():
   logging.basicConfig(level=logging.DEBUG)
   from architectureresource.resource import NetworkAnalyzer
   
-
-
-
-
-
-@click.command()
-@click.argument('file_list', nargs=-1)
-def standard_vgg_training(file_list):
-  """
-    Standard VGG Training is aimed at 
-  """
-  logging.basicConfig(level=logging.DEBUG)
-  logger = logging.getLogger()
-  sess = tf.Session()
-
-
-  generator = MultiFileDataGenerator(file_list, 'image/wires','label/type', batch_size=1)
-  model = VGG16(generator)
-  training_output = model.fit_generator(generator, steps_per_epoch = 1000, 
-                                      epochs=1000)
-  model.save("trained_weights.h5")
-  open('history.json','w').write({'loss':training_output.history['loss'], 
-                                  'accuracy':training_output['accuracy']})
-  logger.info("Done.")
-
-_model = None
-def signal_handler(signal, frame):
-  global _model
-  logging.info("SigINT was called. Saving Model")
-  if _model is not None:
-    _model.save('interrupted_output.h5')
-    logging.info('Model Weights saved to interrupted_output.h5')
-  sys.exit(0)
 
 
 if __name__ == "__main__":
